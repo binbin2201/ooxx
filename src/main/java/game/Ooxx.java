21f57d4fcfcb870c5274ab622a9610ad42fb450b
Zhao <ninjatuna970@gmail.com>
2022-06-23 17:18:53 +0800

    Initial commit

---
+++ src/main/java/game/Ooxx.java
@@ -0,0 +1,22 @@
+package game;
+
+public class Ooxx {
+    public static void main(String[] args) {
+        //2d array 棋盤
+        char[][] board = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+    }
+    public static void printBoard(char[][] board){
+        for(char [] row : board){
+            for(char c : row){
+
+            }
+
+        }
+    }
+}
+
